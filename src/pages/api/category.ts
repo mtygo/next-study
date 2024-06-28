db17563f0a425cfe814134153a4bbc4a23051eb1
wb-mty940435 <wb-mty940435@cainiao.com>
2024-06-28 17:57:06 +0800

    feat: 去掉无用代码

--- src/pages/api/category.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import { NextApiRequest, NextApiResponse } from 'next';
-
-const prisma = new PrismaClient();
-
-export default async function fetchSomeData(req: NextApiRequest, res: NextApiResponse) {
-  const data = await prisma.category.findMany()
-  res.status(200).json({ code: 200, data })
-}
